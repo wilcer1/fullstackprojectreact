f40b6e841797d695a4b4223b644c1cf36da45c1e
wilcer1 <wille.cervin@live.se>
2022-04-22 12:21:54 +0200

    changed running port for express and added  empty js files for future use

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static("build"));
 app.use("/api", routes);
-app.listen(3000, () => {
+app.listen(5000, () => {
     console.log("server running");
 
 });
