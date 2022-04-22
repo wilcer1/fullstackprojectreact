f8d7b393427c9f9542baa2b1ead5a119b12d303a
viggeh2000 <79460359+viggeh2000@users.noreply.github.com>
2022-04-22 12:41:46 +0200

    added connection

---
+++ config/db.config.js
@@ -0,0 +1,21 @@
+require("dotenv").config()
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "34.88.143.247",
+  user: "root",
+  password: process.env.PASSWORD,
+  database: "Gym"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+con.query(`select * from Gym.customer`, (err, res) => {
+    return console.log(res);
+})
