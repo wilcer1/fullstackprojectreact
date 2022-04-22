6492c86f53500a412ce8f47504aac1dfb3397b35
viggeh2000 <79460359+viggeh2000@users.noreply.github.com>
2022-04-22 17:16:37 +0200

    Made EER/ER - diagram & connected our DB to our project

--- config/db.config.js
+++ config/db.config.js
@@ -7,15 +7,23 @@ var con = mysql.createConnection({
   host: "34.88.143.247",
   user: "root",
   password: process.env.PASSWORD,
-  database: "Gym"
+  database: "cinema"
 });
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
+  console.log("Connected to database!");
 });
 
-
-con.query(`select * from Gym.customer`, (err, res) => {
+con.query(`select * from User;`, (err, res) => {
     return console.log(res);
-});
+})
+
+
+
+
+//Select one specifc user
+con.query(`select * from User where FirstName="Lucas" && LastName="booty";`, (err, res) => {
+    return console.log(res);
+})
+
