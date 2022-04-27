2b27ca63b54892439422608363b20d9a4784ccf8
Lucas Carlsson <lucas.carlsson@outlook.com>
2022-04-27 12:09:20 +0200

    Created functionality for color change on booked seats
    Created dummy seats to create functionality. Will be read from database when compontent is finished

--- src/components/CinemaRoom.js
+++ src/components/CinemaRoom.js
@@ -5,14 +5,67 @@ import { useState } from "react"
 
 function CinemaRoom(props){
     const [dataTable, setDataTable] = useState([])
+    const [rows, setRows] = useState([])
     useEffect(() => {
-    const list = []
-    for (let i = 0; i < 15; i++){
-        list.push({data: i})
-    }
-    setDataTable(list)
+        const numberOfRows = 3
+        const rows = []
+        for (let i = 0; i <= numberOfRows; i++){
+            rows.push(i)
+        }
+        const list = [
+            {
+            id: 1, 
+            row: 1,
+            booked: false
+            },
+            {
+            id: 2, 
+            row: 1,
+            booked: false
+            },
+            {
+            id: 3,
+            row: 1,
+            booked: false
+            },
+            {
+            id: 4, 
+            row: 2,
+            booked: false
+            },
+            {
+            id: 5, 
+            row: 2,
+            booked: false
+            },
+            {
+            id: 6, 
+            row: 2,
+            booked: false
+            },
+            {
+            id: 7, 
+            row: 3,
+            booked: true
+            },
+            {
+            id: 8, 
+            row: 3,
+            booked: true
+            },
+            {
+            id: 9, 
+            row: 3,
+            booked: true
+            }
+        ]
+        setDataTable(list)
+        setRows(rows)
 
-}, [])
+    }, [])
+    const test = (id) => {
+        alert(`Clicked button with id ${id}`)
+    }
     return(
         <div>
         <Navbar/>
@@ -21,11 +74,17 @@ function CinemaRoom(props){
             <thead>
             </thead>
             <tbody>
-            {dataTable.map(item => (
+            {rows.map(row => (
             <tr>
-                {dataTable.map(item => (
-                    <td>
-                        <button>{item.data}</button>
+                {dataTable
+                .filter(item => item.row == row)
+                .map(item => (
+                    <td id={item.id}>
+                        <button 
+                            style={item.booked ? {backgroundColor: "red"}: {backgroundColor: "yellowgreen"}} 
+                            onClick={() => test(item.id)}
+                        >{item.id}
+                        </button>
                     </td>
                 ))}
             </tr>
