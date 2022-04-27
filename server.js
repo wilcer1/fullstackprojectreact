c4bd21d6e518e35d546767bf6c5fff7a5cde9d9c
wilcer1 <wille.cervin@live.se>
2022-04-27 13:08:34 +0200

    added bcrypt and jsonwebtoken for login and register.  Started on API for verification

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const routes = require("./routes/routes");
+const authRoute = require('./routes/auth');
 const app = express();
 
 app.use(express.json());
 app.use(express.static("build"));
 app.use("/api", routes);
+app.use("/api/auth", authRoute);
 app.listen(5000, () => {
     console.log("server running");
 
