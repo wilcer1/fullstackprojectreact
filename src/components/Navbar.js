3954c75fcfb9eb61ad5885ea92fb1b6c9b75e22a
Timmy Bergvall <79316874+TimmyBergvall@users.noreply.github.com>
2022-04-28 21:00:12 +0200

    Changed position in navbar
    changed order with sign in and movies

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,8 +39,8 @@ function Navbar(props){
         <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/CinemaRoom">CinemaRoom</a></li>
-            <li><a href="/SignIn">SignIn</a></li>
             <li><a href="/Movies">Movies</a></li>
+            <li><a href="/SignIn">Sign in</a></li>
             <li><a href="/Register">Register</a></li>
             <li className="currentUser"><a>{signedIn ? `Signed in as: ${user}`: "Not signed in"}</a></li>
         </ul>
