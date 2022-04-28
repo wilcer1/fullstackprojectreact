2fd93246c0149a621c2ef7fc7109c89302395b02
viggeh2000 <79460359+viggeh2000@users.noreply.github.com>
2022-04-28 21:47:16 +0200

    added homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,43 @@
-import React from "react";
-import Navbar from "./Navbar";
+import { tab } from "@testing-library/user-event/dist/tab"
+import React from "react"
+import { useState } from "react"
+import { useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import Navbar from "./Navbar"
+
   
 function Home(){
+    const history = useHistory()
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/api/movie")
+        .then(res => res.json())
+        .then(response => {
+            setMovies(response)
+        })
+    }, [])
+
+
   return (
     <div>
       <Navbar/>
-      <h2>Home Page</h2>
+      <div id="homepage">
+
+      <h1>Movies right now!</h1>  
+      <div id = "showcase">
+          {movies.slice(0,3).map(movie => (
+            
+            
+            <img src={movie.Poster}></img>
+          
+
+            ))}
+
+      </div>
     </div>
+  </div>
+
   );
 };
   
