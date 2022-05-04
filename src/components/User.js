42c2af3c609402bf14753362a091e8e0de56e08e
Timmy Bergvall <79316874+TimmyBergvall@users.noreply.github.com>
2022-05-04 21:39:56 +0200

    Started on function user details
    added some styling, and made some small changes in Navbar.js. Now user details as email, first name, last name and birthdate!

--- src/components/User.js
+++ src/components/User.js
@@ -1,14 +1,67 @@
 import React from "react"
 import Navbar from "./Navbar"
+import { useState } from "react"
 
 function User() {
+    const [email, setEmail] = useState([])
+    const [user, setUser] = useState([])
+    const getToken = localStorage.getItem("auth-token")
+
+    if(getToken){
+        const info = {
+        token: getToken
+    }
+
+    fetch("http://localhost:5000/api/auth/user", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(info)
+        })
+        .then(res => res.json())
+        .then(response => {
+            if(response !== "Invalid Token") {
+                setEmail(response)
+            }
+            
+            
+        }).catch(err => console.log(err))  
+    
+
+        fetch(`http://localhost:5000/api/user/${email}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                'Accept': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(response => {
+            setUser(response[0])
+        
+            
+        }).catch(err => console.log(err))  
+
+        
+
     return(
         <div>
             <Navbar/>
+            <div className="userInfo">
+                <h2>User information</h2>
+                <p id="info">Email: {email}</p>
+                <p id="info">First name: {user.FirstName}</p>
+                <p id="info">Last name: {user.LastName}</p>
+                <p id="info">Birthday: {user.BirthDate}</p>
+            </div>
         </div>
+        
+        
     )
 }
-
+}
 
 
 export default User
