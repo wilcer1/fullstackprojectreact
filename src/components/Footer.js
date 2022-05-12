e401e0c66dc02d7a696fc37e46fe307bf2d14d89
viggeh2000 <79460359+viggeh2000@users.noreply.github.com>
2022-05-12 23:47:45 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,22 @@ export default function Footer() {
 <div id="container">
 <div id="cont">
 <div class="footer_center">
-	   <h3>Hello pojkar</h3>
+<div className="about">
+        <h1>Lucas Carlsson</h1>
+        <h3>Frontend Developer</h3>
+        <br></br>
+        <h1>Timmy Bergvall</h1>
+        <h3>Frontend Developer</h3>
+        <br></br>
+        <h1>Victor Hylén</h1>
+        <h3>Database Manager</h3>
+        <br></br>
+        <h1>Wille Cervin</h1>
+        <h3>Backend Developer</h3>
+        <br></br>
+        <h1>Gonzalo Carrero</h1>
+        <h3>About Page</h3>
+    </div>
 </div>
 </div>
 </div>
